c47838b2e4ab5d36f6f9e8eecf39ebed2211e973
y <yuki@test.sakakibara-satomi.info>
2017-10-17 23:18:33 +0900

    init.sh 構造変更

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm -f ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-mkdir -p ~/.dotfiles/.config
-ln -s ~/.dotfiles/.config/nvim ~/.config
+mkdir -p ~/.config
+rm -f ~/.config/nvim
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
