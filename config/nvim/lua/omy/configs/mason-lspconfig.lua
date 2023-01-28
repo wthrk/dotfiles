8ba64bd9cb931f27842616e013751c6227fd3fb3
yuki <yuki@wthrk.jp>
2023-01-29 04:24:26 +0900

    LSP 補完ができた

--- config/nvim/lua/omy/configs/mason-lspconfig.lua
+++ config/nvim/lua/omy/configs/mason-lspconfig.lua
@@ -6,5 +6,9 @@ mason_lspconfig.setup {
 }
 
 mason_lspconfig.setup_handlers {
-  function(server_name) lspconfig[server_name].setup {} end,
+  function(server_name)
+    lspconfig[server_name].setup {
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    }
+  end,
 }
--- config/nvim/lua/omy/configs/nvim-cmp.lua
+++ config/nvim/lua/omy/configs/nvim-cmp.lua
@@ -7,12 +7,3 @@ cmp.setup {
     { name = "nvim_lsp" },
   },
 }
-
-local lspconfig = require "lspconfig"
-local lsp_defaults = lspconfig.util.default_config
-
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-  "force",
-  lsp_defaults.capabilities,
-  require("cmp_nvim_lsp").default_capabilities()
-)
