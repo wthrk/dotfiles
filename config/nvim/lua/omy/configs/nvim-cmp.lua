d17a3410070576e9ddf732e146acad9b65e8414f
ya <ya@yanoMacBook-Air.local>
2023-01-31 08:10:36 +0900

    add cmp-cmdline and config

--- config/nvim/lua/omy/configs/nvim-cmp.lua
+++ config/nvim/lua/omy/configs/nvim-cmp.lua
@@ -3,11 +3,26 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 local cmp = require "cmp"
 
 cmp.setup {
-  sources = cmp.config.sources {
+  sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "nvim_lsp_signature_help" },
-    { name = "buffer" },
     { name = "path" },
     { name = "treesitter" },
-  },
+  }, {
+    { name = "buffer" },
+  }),
 }
+
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, { { name = "cmdline" } }),
+})
--- config/nvim/lua/omy/plugins.lua
+++ config/nvim/lua/omy/plugins.lua
@@ -26,6 +26,7 @@ require("jetpack.packer").add {
   "hrsh7th/cmp-nvim-lsp-signature-help",
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
   "ray-x/cmp-treesitter",
   {
     "hrsh7th/nvim-cmp",
