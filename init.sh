31c1047b5133039f7594bad07f4761b5f66ff2ca
ABE Y <yuki@wthrk.jp>
2023-01-31 16:28:38 +0900

    fix for install

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ rm -f ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 mkdir -p ~/.config
 rm -f ~/.config/nvim
-ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/config/nvim ~/.config/nvim
