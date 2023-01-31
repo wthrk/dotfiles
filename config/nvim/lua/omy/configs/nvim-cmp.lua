8c38359c5efcf4384c50d29a9de872e82e7ac1ae
ya <ya@yanoMacBook-Air.local>
2023-01-31 09:02:29 +0900

    cmp のキー設定を atreonvim を参考に追加

--- config/nvim/lua/omy/configs/nvim-cmp.lua
+++ config/nvim/lua/omy/configs/nvim-cmp.lua
@@ -2,6 +2,20 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 local cmp = require "cmp"
 
+--local border_opts = {
+--  border = "single",
+--  winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+--}
+local function has_words_before()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+    and vim.api
+        .nvim_buf_get_lines(0, line - 1, line, true)[1]
+        :sub(col, col)
+        :match "%s"
+      == nil
+end
+
 cmp.setup {
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
@@ -11,6 +25,63 @@ cmp.setup {
   }, {
     { name = "buffer" },
   }),
+  mapping = {
+    ["<Up>"] = cmp.mapping.select_prev_item {
+      behavior = cmp.SelectBehavior.Select,
+    },
+    ["<Down>"] = cmp.mapping.select_next_item {
+      behavior = cmp.SelectBehavior.Select,
+    },
+    ["<C-p>"] = cmp.mapping.select_prev_item {
+      behavior = cmp.SelectBehavior.Insert,
+    },
+    ["<C-n>"] = cmp.mapping.select_next_item {
+      behavior = cmp.SelectBehavior.Insert,
+    },
+    ["<C-k>"] = cmp.mapping.select_prev_item {
+      behavior = cmp.SelectBehavior.Insert,
+    },
+    ["<C-j>"] = cmp.mapping.select_next_item {
+      behavior = cmp.SelectBehavior.Insert,
+    },
+    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable,
+    ["<C-e>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    ["<CR>"] = cmp.mapping.confirm { select = false },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+  },
+  --window = {
+  --  completion = cmp.config.window.bordered(border_opts),
+  --  documentation = cmp.config.window.bordered(border_opts),
+  --},
 }
 
 cmp.setup.cmdline({ "/", "?" }, {
