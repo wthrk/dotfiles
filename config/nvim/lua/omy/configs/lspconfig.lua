d6841a4d3dda1c8dff647e0bdc306905f857b2ed
ya <ya@yanoMacBook-Air.local>
2023-02-06 08:12:02 +0900

    rust-analyzer の設定を追加

--- config/nvim/lua/omy/configs/lspconfig.lua
+++ config/nvim/lua/omy/configs/lspconfig.lua
@@ -14,4 +14,30 @@ return {
       on_attach = on_attach,
     }
   end,
+
+  rust_analyzer = function(cababilities, on_attach)
+    lspconfig.rust_analyzer.setup {
+      settings = {
+        ["rust-analyzer"] = {
+          imports = {
+            granularity = {
+              group = "module",
+            },
+            prefix = "self",
+          },
+          cargo = {
+            buildScripts = {
+              enable = true,
+            },
+            features = "all",
+          },
+          procMacro = {
+            enable = true,
+          },
+        },
+      },
+      cababilities = cababilities,
+      on_attach = on_attach,
+    }
+  end,
 }
