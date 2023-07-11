75cd522e23c2c3ad3c4a8732e2faf3d60c00ad20
EC2 Default User <ec2-user@ip-172-31-23-141.ap-northeast-1.compute.internal>
2023-07-11 06:44:06 +0000

    mason-null-ls 設定方法変更に基づく修正

--- config/nvim/lua/omy/configs/mason-null-ls.lua
+++ config/nvim/lua/omy/configs/mason-null-ls.lua
@@ -2,12 +2,7 @@ local mason_null_ls = require "mason-null-ls"
 
 mason_null_ls.setup {
   ensure_installed = { "stylua" },
-}
 
-mason_null_ls.setup_handlers {
-  function(source_name, methods)
-    require "mason-null-ls.automatic_setup"(source_name, methods)
-  end,
 }
 
 require "omy.configs.null-ls"
