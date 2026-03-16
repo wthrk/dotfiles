6b211d3e1eaa4a16232c8ea867707eb0f0a056d8
ABE Y. <yuki@wthrk.jp>
2026-03-16 18:53:31 +0900

    Track powerlevel10k config in dotfiles

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -1,4 +1,4 @@
 # powerlevel10k configuration
-if [[ -o interactive ]] && [[ -t 0 ]] && [[ -t 1 ]] && [[ -r "$HOME/.p10k.zsh" ]]; then
-  source "$HOME/.p10k.zsh"
+if [[ -o interactive ]] && [[ -t 0 ]] && [[ -t 1 ]] && [[ -r "$HOME/.config/zsh/p10k.zsh" ]]; then
+  source "$HOME/.config/zsh/p10k.zsh"
 fi
