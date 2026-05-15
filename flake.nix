34d44b9bdd92bb2165a4d4ec606e7d1af99b3c13
ABE Y. <yuki@wthrk.jp>
2026-05-15 16:12:20 +0900

    fix(nix): Linux CIでpcsclite依存を解決する

--- flake.nix
+++ flake.nix
@@ -196,7 +196,11 @@
             "--package"
             "dotfiles-cli"
           ];
-          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.pcsclite ];
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+            pkgs.pkg-config
+          ];
           postInstall = ''
             wrapProgram $out/bin/dotfiles \
               --set-default DOTFILES_HOME_MANAGER ${
@@ -229,6 +233,8 @@
             pkgs.nil
             pkgs.nix
             pkgs.nixd
+            pkgs.pcsclite
+            pkgs.pkg-config
             pkgs.ripgrep
             pkgs.rust-analyzer
             pkgs.rustc
