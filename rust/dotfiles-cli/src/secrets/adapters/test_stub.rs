63b12c0423da90bb89e689b6261ddb2ac2b87043
ABE Y. <yuki@wthrk.jp>
2026-05-24 21:06:51 +0900

    Revert "chore(yubikey): capture step1 attempt (V7 revert needed)"
    This reverts commit 1e05bb8c6f6eefa534db8a865a51094dbb51f588.

--- rust/dotfiles-cli/src/secrets/adapters/test_stub.rs
+++ rust/dotfiles-cli/src/secrets/adapters/test_stub.rs
@@ -10,8 +10,7 @@ use clap::{Parser, ValueEnum};
 
 use crate::secrets::{
     blob::{decrypt_secret_protected, encrypt_secret},
-    domain::{self, SecretBlob, SecretManifest, SecretName},
-    ports::SecretDevice,
+    domain::{self, SecretBlob, SecretDevice, SecretManifest, SecretName},
     support::protection::{ProtectedSecret, SecretSession},
 };
 use crate::Result;
@@ -344,8 +343,9 @@ impl SecretDevice for TestDevice {
         self.config.read_pin_from_tty
     }
 
-    fn unwrap_key(&mut self, wrapped_key: &[u8]) -> Result<Vec<u8>> {
-        self.wrap_key(wrapped_key)
+    fn write_unwrapped_key(&mut self, wrapped_key: &[u8], output: &mut impl Write) -> Result<()> {
+        output.write_all(&self.wrap_key(wrapped_key)?)?;
+        Ok(())
     }
 }
 
